0fe721198202f31407d475bd4ec3ca8c79f6e30e
FajarAdityaNugraha <fajaradityanugraha01@gmail.com>
2023-11-09 15:58:06 +0700

    Perulangan 2 Individu 1

---
+++ Individu1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Individu1 {
+    public static void main(String[] args) {
+        Scanner input10 = new Scanner(System.in);
+        System.out.print("Masukkan nilai N= ");
+        int N = input10.nextInt();
+        if (N < 3) {
+            System.out.println("Nilai N harus minimal 3.");
+        } else {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N - i; j++) {
+                    System.out.print(" ");
+                }
+                for (int k = 1; k <= i; k++) {
+                    System.out.print(k);
+                }
+                System.out.println();
+            }
+        }
+    }
+}
